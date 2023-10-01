e57a4afec5e5219d6365a7d4e485825210cdf40f
Theodor A.W <ctweiden@math.uio.no>
2023-10-01 21:40:35 +0200

    Only testing remaning

---
+++ assignment3/in3110_instapy/numba_filters.py
@@ -0,0 +1,53 @@
+"""numba-optimized filters"""
+from __future__ import annotations
+
+import numpy as np
+from numba import jit
+
+@jit(nopython = True)
+def numba_color2gray(image: np.array) -> np.array:
+    """Convert rgb pixel array to grayscale
+
+    Args:
+        image (np.array)
+    Returns:
+        np.array: gray_image
+    """
+    rows, cols, _ = image.shape
+    gray_image = np.empty((rows, cols), dtype = np.float64)
+    # iterate through the pixels, and apply the grayscale transform
+ 
+    for i in range(rows):
+        for j in range(cols):
+            r, g, b = image[i, j]
+            gray = int(r * 0.21 + g * 0.72 + b * 0.07)
+            gray_image[i, j] = gray
+    ...
+
+    gray_image = gray_image.astype(np.uint8)
+
+    return gray_image
+
+@jit(nopython = True)
+def numba_color2sepia(image: np.array) -> np.array:
+    """Convert rgb pixel array to sepia
+
+    Args:
+        image (np.array)
+    Returns:
+        np.array: sepia_image
+    """
+    sepia_image = np.empty_like(image)
+    height, width, _ = image.shape
+    # Iterate through the pixels
+    for y in range(height):
+        for x in range(width):
+            r, g, b = image[y, x]
+    # applying the sepia matrix
+            tr = int(0.393 * r + 0.769 * g + 0.189 * b)
+            tg = int(0.349 * r + 0.686 * g + 0.168 * b)
+            tb = int(0.272 * r + 0.534 * g + 0.131 * b)
+            sepia_image[y, x] = [min(255, tr), min(255, tg), min(255, tb)]
+    # Return image
+    # don't forget to make sure it's the right type!
+    return sepia_image
---
+++ assignment3/in3110_instapy/numpy_filters.py
@@ -0,0 +1,89 @@
+"""numpy implementation of image filters"""
+from __future__ import annotations
+from PIL import Image
+import numpy as np
+
+
+def numpy_color2gray(image: np.array) -> np.array:
+    """Convert rgb pixel array to grayscale
+
+    Args:
+        image (np.array)
+    Returns:
+        np.array: gray_image
+    """
+
+    gray_image = np.empty_like(image)
+
+    # Hint: use numpy slicing in order to have fast vectorized code
+
+    gray_image = (image[:,:,0] * 0.21 + image[:,:,1] * 0.72 + image[:,:,2] * 0.07).astype(np.uint8)
+
+    # Return image (make sure it's the right type!)
+    return gray_image
+
+
+def numpy_color2sepia(image: np.array, k: float = 1) -> np.array:
+    """Convert rgb pixel array to sepia
+
+    Args:
+        image (np.array)
+        k (float): amount of sepia (optional)
+
+    The amount of sepia is given as a fraction, k=0 yields no sepia while
+    k=1 yields full sepia.
+
+    Returns:
+        np.array: sepia_image
+    """
+    if not 0 <= k <= 1:
+        # validate k
+        raise ValueError(f"k must be between [0-1], got {k=}")
+
+    # Initialize sepia_image
+    sepia_image = np.empty_like(image, dtype=np.uint8)
+
+    # Define sepia matrix
+    base_matrix = np.array([
+    [0.393, 0.769, 0.189],
+    [0.349, 0.686, 0.168],
+    [0.272, 0.534, 0.131]
+    ])
+
+    # Incorporate the k factor. When k=1, sepia_matrix = base_matrix. 
+    # When k=0, sepia_matrix is an identity matrix (no change to image).
+    sepia_matrix = k * base_matrix + (1 - k) * np.identity(3)
+
+    # Apply the matrix filter using Einstein summation for matrix multiplication
+    sepia_image = np.einsum('...i,ij->...j', image, sepia_matrix.T) # Transponerer matrisen
+    
+    # Clip the values between 0 and 255
+    sepia_image = np.clip(sepia_image, 0, 255).astype(np.uint8)
+
+    # Return the sepia_image
+    return sepia_image
+'''
+if __name__ == "__main__":
+    input_image_array = np.array(Image.open("test/rain.jpg"))
+   
+    gray_array = numpy_color2gray(input_image_array)
+
+    image = Image.fromarray(gray_array, "L")  # "L" mode for grayscale images
+    image.save("rain_gray.jpg")
+'''
+import matplotlib.pyplot as plt
+if __name__ == "__main__":
+    input_image_array = np.array(Image.open("test/rain.jpg"))
+    sepia_array = numpy_color2sepia(input_image_array)
+
+    # Save the sepia image
+    image = Image.fromarray(sepia_array.astype('uint8'), "RGB")  # "RGB" mode for color images
+    image.save("rain_sepia.jpg")
+
+    reloaded_image = Image.open("rain_sepia.jpg")
+    plt.imshow(reloaded_image)
+    plt.show()
+
+
+
+   
